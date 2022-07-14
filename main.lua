0cd7ced51ba2c13d2e1029faf4f7303d5a91f4f2
Adarsh Singh <hsrada23@gmail.com>
2022-07-14 11:20:08 +0530

    Shading works now

--- main.lua
+++ main.lua
@@ -20,6 +20,12 @@ function vec.__add(v1, v2)
     v1.z + v2.z)
 end
 
+function vec.__mul(v, v1)
+    return vec:new(v1.x * v, 
+    v1.y  * v,
+    v1.z  * v)
+end
+
 
 function vec.__sub(v1, v2)
     return vec:new(v1.x - v2.x, 
@@ -55,8 +61,8 @@ end
 -- SPHERE FUNCTIONS
 sphere = {}
 
-function sphere:new(a,b,c,rad)
-    newObj = {x=a, y=b, z=c, r=rad}
+function sphere:new(v,rad)
+    newObj = {vec=v, r=rad}
     self.__index = self
     return setmetatable(newObj, self)
 end
@@ -64,12 +70,12 @@ end
 
 function sphere:intersects(v)
     --d = cross(v,vec:new(self.x, self.y, self.z)):magnitude()/v:magnitude()
-    c = vec:new(-self.x, -self.y, -self.z) -- center of the sphere
+    c = self.vec -- center of the sphere
     te = dot(c,v)
     vv = dot(v,v)
     d = te * te - vv * (dot(c,c) - self.r*self.r)
     if d > 0 then
-        return -(2*te - math.sqrt(d))/(2*vv)
+        return (2*te - math.sqrt(d))/(2*vv)
     else
         return -1
     end
@@ -87,19 +93,36 @@ function bgcolor(h)
 end
 
 
-s = sphere:new(0,0,600,50)
-t = vec:new(159,106,0)
+s = sphere:new(vec:new(0,0,600),50)
+t = vec:new(159,-106,0)
+
+l = vec:new(-300,400,50)
+
+
 
 
 local b = Bitmap:new(318, 212)                  -- Remove for calculator
 b:clear({255,255,255})                          -- Remove for calculator
 for px = 0, 318 do
     for py=0, 212 do
-        pv = vec:new(px,py,400)
+        pv = vec:new(px,-py,400) - t
 
-        dist = s:intersects(pv-t)
+        dist = s:intersects(pv)
         if  dist ~= -1 then
-            pcolor = {255,0,255}
+
+            norm = dist*(pv) - s.vec
+            --print(norm.x,norm.y,norm.z)
+            alp = (dot(norm:normalize(), (l-s.vec):normalize()))
+            if alp < 0 then
+                alp = 0
+            else 
+                alp = math.abs(alp)
+            end
+            --print(alp)
+
+            pcolor = {math.floor(255*alp),0,math.floor(255*alp)}
+
+
             
             --gc:setColorRGB(unpack(pcolor))
         else
